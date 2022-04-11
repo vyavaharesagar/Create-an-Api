ae9618c3165ed8fcc79a5f8d529c4e42b0848fb2
Sagar V <sagarv@leotechnosoft.net>
2022-04-11 14:18:02 +0530

    finished

--- test.py
+++ test.py
@@ -1,11 +1,23 @@
-from urllib import response
+from django import views
 import requests
+
 
 BASE = "http://127.0.0.1:5000/"
 
-# response = requests.put(BASE + "video/1",{"likes":10, "name":"tim","views":100000})
-# print(response.json())
+
+# data = [{"likes":335, "name":"tim","views":200000},
+#         {"likes":40, "name":"John","views":300000},
+#         {"likes":205, "name":"Flem","views":100000}]
+
+# for i in range(len(data)):
+#     response = requests.put(BASE+"video/" + str(i), data[i])
+#     print(response.json())
+
+# input()
+# response = requests.delete(BASE + "video/0")
+# print(response)
+
 # input()
 
-response = requests.get(BASE + "video/6")
+response = requests.patch(BASE + "video/2", {"views":99, "likes":101})
 print(response.json())
